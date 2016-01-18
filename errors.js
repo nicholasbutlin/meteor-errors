9e3ab478be1e991eb3755e33e0a4409f55f566d7
nicholasbutlin <nicholasbutlin@gmail.com>
2016-01-18 14:46:19 +0000

    updated

--- errors.js
+++ errors.js
@@ -2,7 +2,7 @@ Errors = {
   // Local (client-only) collection
   collection: new Mongo.Collection(null),
 
-  throw: function(message) {
+  throw: function(message, alertType) {
     Errors.collection.insert({message: message, alert: alertType, seen: false})
   }
 };
